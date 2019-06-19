dfa330f5a545707f3d30f6c6e38628fd2b12efbd
Julieta <juliponti@hotmail.com>
2019-06-19 14:41:49 -0300

    Update ariel.py

--- ariel.py
+++ ariel.py
@@ -1,76 +1,59 @@
-import geopy
-from geopy import distance
+#import geopy
+#from geopy import distance
 
 def calcular_distancia():
     newport_ri = (41.49008, -71.312796)
     cleveland_oh = (41.499498, -81.695391)
-    print(distance.distance(newport_ri, cleveland_oh).kilometres)
+    #print(distance.distance(newport_ri, cleveland_oh).kilometres)
 
-
-
-
-from random import randint, uniform, choice
+#calcular_distancia()
 def leer_archivo(archivo, vacio):
     linea = archivo.readline() #guarda una cadena de caracteres del archivo
     if linea:
         lista = linea.rstrip().split(",")
-        return (lista)
+        if lista[2] == 'dni':
+            return leer_archivo(archivo, vacio)
+        return lista[0], lista[1],int(lista[2]),lista[3]
     else:
         return (vacio)
 
-def recorrer_archivo(direccion):
-    archivo = open(direccion,'r', encoding='utf-8')
-    vacio = []
-    informacion_archivo = []
-    datos_linea = leer_archivo(archivo,vacio)
-    cont = 0
-    while datos_linea:
-        if cont != 0:
-            informacion_archivo.append(datos_linea)
-        cont += 1
-        datos_linea = leer_archivo(archivo,vacio)    
-    archivo.close()
-    return informacion_archivo
+def merge_usuarios():
+    usuarios1 = open('usuarios1.csv','r',encoding = 'utf-8')
+    usuarios2 = open('usuarios2.csv','r',encoding = 'utf-8')
+    usuarios3 = open('usuarios3.csv','r',encoding = 'utf-8')
+    usuarios4 = open('usuarios4.csv','r',encoding = 'utf-8')
+    nombre1,celular1,dni1,pin1 = leer_archivo(usuarios1, [0,0,999999999,0])
+    nombre2,celular2,dni2,pin2 = leer_archivo(usuarios2,[0,0,999999999,0])
+    nombre3,celular3,dni3,pin3 = leer_archivo(usuarios3, [0,0,999999999,0])
+    nombre4,celular4,dni4,pin4 = leer_archivo(usuarios4, [0,0,999999999,0])
 
-
-def lectura_estaciones(direccion):
-    lista_estaciones = recorrer_archivo("estaciones.csv")
-    estaciones = {}
-    for datos_estacion in lista_estaciones:
-        #longitud,latitud,direccion,capacidad, bicicletas_ancladas, cantidad_usos
-        bicicletas_ancladas = []
-        cantidad_usos_estacion = 0
-        estaciones[int(datos_estacion[3])] = [datos_estacion[0],datos_estacion[1],datos_estacion[2],datos_estacion[4], bicicletas_ancladas,cantidad_usos_estacion]
-    return estaciones
+    maestro_usuarios = open('usuarios.csv','w',encoding = 'utf-8')
     
-def lectura_bicicletas(direccion,estaciones):
-    lista_bicicletas = recorrer_archivo("bicicletas.csv")
-    bicicletas = {}
-    #estado,ubicacion
-    estado  = "ok"
-    ubicacion = "anclada"
-    numeros_estaciones =[]
-    for estacion in estaciones:
-        numeros_estaciones.append(estacion)
-    for datos_bicicleta in lista_bicicletas:
-        bicicletas[datos_bicicleta[1]] = [estado,ubicacion]
-        posicion_estacion = randint(0,len(numeros_estaciones)-1)
-        numero_estacion = numeros_estaciones[posicion_estacion]
-        while (len(estaciones[numero_estacion][4]) >= 30):
-            numero_estacion = randint(0,len(numeros_estaciones))
-        estaciones[numero_estacion][4].append(datos_bicicleta[1])
-    return bicicletas, estaciones
+    
+    while dni1 !=999999999 or dni2 !=999999999 or dni3 !=999999999 or dni4 !=999999999:
+        menor = min(int(dni1),int(dni2),int(dni3),int(dni4))
+        while menor  == dni1 and dni1 !=0:
+            linea = "{},{},{},{} \n".format(nombre1,celular1,dni1,pin1)
+            maestro_usuarios.write(linea)
+            nombre1,celular1,dni1,pin1 = leer_archivo(usuarios1,[0,0,999999999,0])
+        while menor  == dni2 and dni2 !=0:
+            linea = "{},{},{},{} \n".format(nombre2,celular2,dni2,pin2)
+            maestro_usuarios.write(linea)
+            nombre2,celular2,dni2,pin2 = leer_archivo(usuarios2,[0,0,999999999,0])
+        while menor  == dni3 and dni3 !=0:
+            linea = "{},{},{},{} \n".format(nombre3,celular3,dni3,pin3)
+            maestro_usuarios.write(linea)
+            nombre3,celular3,dni3,pin3= leer_archivo(usuarios3,[0,0,999999999,0])
+        while menor  == dni4 and dni4 !=0:
+            linea = "{},{},{},{} \n".format(nombre4,celular4,dni4,pin4)
+            maestro_usuarios.write(linea)
+            nombre4,celular4,dni4,pin4 = leer_archivo(usuarios4,[0,0,999999999,0])
+        
+    usuarios1.close()
+    usuarios2.close()
+    usuarios3.close()
+    usuarios4.close()
+    maestro_usuarios.close()
+    
 
-def lectura_archivos():
-    estaciones =  lectura_estaciones('estaciones.csv')
-    bicicletas, estaciones = lectura_bicicletas ('bicicletas.csv',estaciones)
-    return estaciones, bicicletas
-
-#estaciones, bicicletas =  lectura_archivos()   
-"""
-for estacion in estaciones:
-    print (len(estaciones[estacion][4]))
-    print (estaciones[estacion][3])
-#print(bicicletas)
-"""
-calcular_distancia()
+merge_usuarios()
